83b7df2221b0b595ed58aa19c57f67909cd39567
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-11-09 03:42:25 -0300

    feat: add recommendedTrips components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,7 @@ const config: Config = {
         textColor: {
             dark: '#717171',
             primary: '#590BD8',
+            primaryDarker: '#312A4F',
         },
     },
     plugins: [],
