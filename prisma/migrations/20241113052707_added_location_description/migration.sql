49a3b4adc92c7a6c3f40037228ba9dc5802a898e
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-11-13 02:48:42 -0300

    feat: added locationDescription to schema

---
+++ prisma/migrations/20241113052707_added_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse hendrerit auctor nisl non blandit. Aliquam consectetur enim eu ullamcorper feugiat. Quisque lectus risus, semper at lectus nec, condimentum pharetra tellus.';
