6f53de74dca78214d0f3b9a859e21872d3e36867
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-11-25 09:56:57 -0300

    feat: add toast on tripConfirmation and updated schema

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -3,65 +3,95 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
-import ReactCountryFlag from 'react-country-flag';
 import { format } from 'date-fns';
+import ReactCountryFlag from 'react-country-flag';
 import { ptBR } from 'date-fns/locale';
-import Button from '@/components/Button';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
+import Button from '@/components/Button';
+
+import { Trip } from '@prisma/client';
+import { toast } from 'react-toastify';
 const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
-    const [trip, setTrip] = useState();
+    const [trip, setTrip] = useState<Trip | null>();
     const [totalPrice, setTotalPrice] = useState<number>(0);
-    const searchParams = useSearchParams();
+
+    const router = useRouter();
+
     const { status, data } = useSession();
-    const router = useRouter()
+
+    const searchParams = useSearchParams();
 
     useEffect(() => {
         const fetchTrip = async () => {
-            const response = await fetch(
-                `http://localhost:3000/api/trips/check`,
-                {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        tripId: params.tripId,
-                        startDate: searchParams.get('startDate'),
-                        endDate: searchParams.get('endDate'),
-                    }),
-                },
-            );
+            const response = await fetch(`/api/trips/check`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    tripId: params.tripId,
+                    startDate: searchParams.get('startDate'),
+                    endDate: searchParams.get('endDate'),
+                }),
+            });
 
             const res = await response.json();
 
             if (res?.error) {
-                return router.push("/");
+                return router.push('/');
             }
 
-            const { trip, totalPrice } = await response.json();
-            setTrip(trip);
-            setTotalPrice(totalPrice);
+            setTrip(res.trip);
+            setTotalPrice(res.totalPrice);
         };
 
-        if (status === "unauthenticated") {
-            router.push("/");
-          }
+        if (status === 'unauthenticated') {
+            router.push('/');
+        }
 
         fetchTrip();
-    }, [status]);
+    }, [status, searchParams, params, router]);
 
-    console.log(trip);
+    if (!trip) return null;
+
+    const handleBuyClick = async () => {
+        const res = await fetch('/api/trips/reservation', {
+            method: 'POST',
+            body: Buffer.from(
+                JSON.stringify({
+                    tripId: params.tripId,
+                    startDate: searchParams.get('startDate'),
+                    endDate: searchParams.get('endDate'),
+                    guests: Number(searchParams.get('guests')),
+                    userId: (data?.user as any).id!,
+                    totalPaid: totalPrice,
+                }),
+            ),
+        });
+
+        if (!res.ok) {
+            return toast.error('Ocorreu um erro ao realizar a reserva!', {
+                position: 'bottom-center',
+            });
+        }
+        
+        router.push("/")
+
+        toast.success('Reserva realizada com sucesso!', {
+            position: 'bottom-center',
+        });
+    };
 
     const startDate = new Date(searchParams.get('startDate') as string);
     const endDate = new Date(searchParams.get('endDate') as string);
     const guests = searchParams.get('guests');
 
-    if (!trip) return null;
     return (
-        <div className="container mx-auto p-5">
+        <div className="container mx-auto p-5 lg:max-w-[600px]">
             <h1 className="font-semibold text-xl text-primaryDarker">
                 Sua viagem
             </h1>
 
+            {/* CARD */}
             <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
                 <div className="flex items-center gap-3 pb-5 border-b border-grayLighter border-solid">
                     <div className="relative h-[106px] w-[124px]">
@@ -89,6 +119,7 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
                         </div>
                     </div>
                 </div>
+
                 <h3 className="font-semibold text-lg text-primaryDarker mt-3">
                     Informações sobre o preço
                 </h3>
@@ -99,20 +130,20 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
                 </div>
             </div>
 
-            <div className='flex flex-col mt-5'>
-              <h3 className='font-semibold text-primaryDarker'>Data</h3>
-              <div className='flex gap-1 mt-1'>
-              <p>{format(startDate, "dd 'de' MMMM", {locale: ptBR})}</p>
-              {" - "}
-              <p>{format(endDate, "dd 'de' MMMM", {locale: ptBR})}</p>
-              </div>
+            <div className="flex flex-col mt-5 text-primaryDarker">
+                <h3 className="font-semibold">Data</h3>
+                <div className="flex items-center gap-1 mt-1">
+                    <p>{format(startDate, "dd 'de' MMMM", { locale: ptBR })}</p>
+                    {' - '}
+                    <p>{format(endDate, "dd 'de' MMMM", { locale: ptBR })}</p>
+                </div>
 
-              <div className='mt-5'>
-                <h3 className='font-semibold text-primaryDarker'>Hóspedes</h3>
+                <h3 className="font-semibold mt-5">Hóspedes</h3>
                 <p>{guests} hóspedes</p>
-              </div>
 
-              <Button className='mt-5'>Finalizar compra</Button>
+                <Button className="mt-5" onClick={handleBuyClick}>
+                    Finalizar Compra
+                </Button>
             </div>
         </div>
     );
