dbd322d04caf088ca0728435547c28671efd8d34
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-12-02 01:53:54 -0300

    fix: eslint adjustment

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
 
-import {prisma} from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -12,8 +12,11 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-  ],callbacks: {
-    async session({ session, user }) {
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async session({ session, token, user }) {
       session.user = { ...session.user, id: user.id } as { id: string; name: string; email: string };
 
       return session;
